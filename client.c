85bd23966c2bcce094978c430cba88718878f908
cronosun <simon.ruoss@gmail.com>
2021-05-14 00:39:07 +0200

    format

--- client.c
+++ client.c
@@ -3,23 +3,26 @@
 #include <alloca.h>
 #include <stdlib.h>
 
-void information_about_basket(Basket basket) {
+void information_about_basket(Basket basket)
+{
     float computed_price = basket_compute_price(basket);
     int number_of_items_in_basket = basket_number_of_items(basket);
     float average_price = computed_price / (float)number_of_items_in_basket;
     printf("Price of all items in the shopping basket is %f CHF; there are %i "
-    "items in the basket; average price is %f CHF.\n", 
-    computed_price, number_of_items_in_basket, average_price);
+           "items in the basket; average price is %f CHF.\n",
+           computed_price, number_of_items_in_basket, average_price);
 }
 
-void fill_the_basket_with_items(Basket basket) {
+void fill_the_basket_with_items(Basket basket)
+{
     basket_add(basket, 21412, 15.30);
     basket_add(basket, 3122, 4.20);
     basket_add(basket, 212, 9.00);
     basket_add(basket, 2412341, 48.30);
 }
 
-void stack_allocated() {
+void stack_allocated()
+{
     // stack allocate basket
     Basket basket = alloca(basket_size());
     // .. and init
@@ -28,7 +31,8 @@ void stack_allocated() {
     information_about_basket(basket);
 }
 
-void heap_allocated() {
+void heap_allocated()
+{
     // heap allocate the basket
     Basket basket = malloc(basket_size());
     // .. and init
@@ -38,7 +42,8 @@ void heap_allocated() {
     free(basket);
 }
 
-int main() {
+int main()
+{
     stack_allocated();
     // the same also works with heap allocation
     heap_allocated();
--- library.c
+++ library.c
@@ -2,36 +2,45 @@
 #include "library.h"
 #include <stddef.h>
 
-int const basket_max_items = 30; 
+int const basket_max_items = 30;
 
-struct BasketItem {
+struct BasketItem
+{
     uint64_t item_id;
     float price;
 };
 
-struct BasketImplementation {
+struct BasketImplementation
+{
     struct BasketItem items[basket_max_items];
     int number_of_items;
 };
 
-struct BasketImplementation* basket_cast(Basket self) {
-    return (struct BasketImplementation*)self;
+struct BasketImplementation *basket_cast(Basket self)
+{
+    return (struct BasketImplementation *)self;
 }
 
-size_t basket_size() {
+size_t basket_size()
+{
     return sizeof(struct BasketImplementation);
 }
 
-void basket_init(Basket self) {
+void basket_init(Basket self)
+{
     struct BasketImplementation *cast = basket_cast(self);
     cast->number_of_items = 0;
 }
 
-BasketAddSuccess basket_add(Basket self, uint64_t item_id, float price) {
+BasketAddSuccess basket_add(Basket self, uint64_t item_id, float price)
+{
     struct BasketImplementation *cast = basket_cast(self);
-    if (cast->number_of_items>=basket_max_items) {
+    if (cast->number_of_items >= basket_max_items)
+    {
         return false;
-    } else {
+    }
+    else
+    {
         cast->items[cast->number_of_items].item_id = item_id;
         cast->items[cast->number_of_items].price = price;
         cast->number_of_items += 1;
@@ -39,17 +48,19 @@ BasketAddSuccess basket_add(Basket self, uint64_t item_id, float price) {
     }
 }
 
-float basket_compute_price(Basket self) {
-        struct BasketImplementation *cast = basket_cast(self);
+float basket_compute_price(Basket self)
+{
+    struct BasketImplementation *cast = basket_cast(self);
     float price_sum = 0;
-    for (int i=0; i<cast->number_of_items; i++) {
+    for (int i = 0; i < cast->number_of_items; i++)
+    {
         price_sum += cast->items[i].price;
     }
     return price_sum;
 }
 
-int basket_number_of_items(Basket self) {
-            struct BasketImplementation *cast = basket_cast(self);
+int basket_number_of_items(Basket self)
+{
+    struct BasketImplementation *cast = basket_cast(self);
     return cast->number_of_items;
 }
-
